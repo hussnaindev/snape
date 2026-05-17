f00e5e2c7783905d3a831f21df7b01a787836be4
hussnaindev <hussnain444all@gmail.com>
2026-05-17 18:21:33 +0500

    Switch to peachify embed player

--- lib/vsembed.ts
+++ lib/vsembed.ts
@@ -1,7 +1,9 @@
 export function getMovieEmbedUrl(tmdbId: number): string {
-  return `/api/embed/movie/${tmdbId}`;
+  // return `/api/embed/movie/${tmdbId}`;
+  return `https://peachify.top/embed/movie/${tmdbId}`;
 }
 
 export function getSeriesEmbedUrl(tmdbId: number, season: number, episode: number): string {
-  return `/api/embed/series/${tmdbId}/${season}/${episode}`;
+  // return `/api/embed/series/${tmdbId}/${season}/${episode}`;
+  return `https://peachify.top/embed/tv/${tmdbId}/${season}/${episode}`;
 }
