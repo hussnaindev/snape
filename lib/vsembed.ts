a1e13b0a7b7b314d1d51348dec45fc984d972332
hussnaindev <hussnain444all@gmail.com>
2026-05-17 23:05:21 +0500

    add accent and cast params to peachify embed URLs

--- lib/vsembed.ts
+++ lib/vsembed.ts
@@ -1,7 +1,7 @@
 export function getMovieEmbedUrl(tmdbId: number): string {
-  return `https://peachify.top/embed/movie/${tmdbId}`;
+  return `https://peachify.top/embed/movie/${tmdbId}?accent=E50914&cast=hide`;
 }
 
 export function getSeriesEmbedUrl(tmdbId: number, season: number, episode: number): string {
-  return `https://peachify.top/embed/tv/${tmdbId}/${season}/${episode}`;
+  return `https://peachify.top/embed/tv/${tmdbId}/${season}/${episode}?accent=E50914&cast=hide`;
 }
